bf08fd60c0256438dfa6d306ccbe4dc6d1e6e267
Nguyễn Vinh <115027531+VinhBot@users.noreply.github.com>
2023-01-29 12:10:34 +0000

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 function App() {
   return (
-    <main style={{ background: "black" }}>
-      <div style={{ color: "red" }}>Địt con mẹ mày thằng mặt lồn cường</div>
+    <>
+    <main style={{ background: "blueviolet" }}>
+      <div style={{ color: "red" }}>Ấn vào xem con đĩ mẹ mày à ? Thấy ngu chưa thằng thiểu năng</div>
     </main>
+    <footer>
+      <img 
+        src={"https://file.vfo.vn/hinh/2018/02/hinh-cho-cuoi-deu-cho-de-thuong-13.jpg"} 
+        alt=""
+        />
+    </footer>
+    </>
   )
 }
 
